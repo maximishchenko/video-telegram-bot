3b99010a107b14a8c9e00af64c51810abe74f225
GORODAVTO\MAX <maxim.ishchenko@gmail.com>
2019-12-28 18:32:37 +0300

    Custom Login Controller

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Shared;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
@@ -39,13 +41,53 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
-    public function authenticated(Request $request, $user)
+    public function showLoginForm()
     {
-        if($user->status != Shared::STATUS_ACTIVE) {
-            $this->guard()->logout();
-            flash(trans('messages.flash_need_email_confirm_account'))->error();
-            return back();
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        if($this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+            $this->sendLockoutResponse();
         }
-        return redirect()->intended($this->redirectPath());
+
+        $authenticate = Auth::attempt(
+            $request->only(['email', 'password']),
+            $request->filled('remember')
+        );
+
+        if($authenticate) {
+            $request->session()->regenerate();
+            $this->clearLoginAttempts($request);
+            $user = Auth::user();
+            if($user->status != Shared::STATUS_ACTIVE) {
+                Auth::logout();
+                flash(trans('messages.flash_need_email_confirm_account'))->error();
+                return back();
+            }
+            return redirect()->intended(route('cabinet'));
+        }
+
+        $this->incrementLoginAttempts($request);
+        throw ValidationException::withMessages(['email' => [trans('auth.failed')]]);
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::guard()->logout();
+        $request->session()->invalidate();
+        return redirect()->route('home');
+    }
+
+    protected function username()
+    {
+        return 'name';
     }
 }
